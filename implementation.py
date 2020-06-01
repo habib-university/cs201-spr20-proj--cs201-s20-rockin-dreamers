5f2259298b25346e3e2c5f56108f6ad7c05a2f4e
m-usaid <54721503+m-usaid@users.noreply.github.com>
2020-06-01 23:15:16 +0500

    basic structure

---
+++ implementation.py
@@ -0,0 +1,20 @@
+class Rope():
+    def __init__(self):
+        self.right = None
+        self.left = None
+        self.weight = 0
+
+    def delete(self):
+        pass
+
+    def concatenate(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def search(self):
+        pass
+
+    def split(self):
+        pass
