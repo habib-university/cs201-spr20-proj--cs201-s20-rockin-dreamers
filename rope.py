82c70eff5c236c99b01f96e0512b3e5bbe45fd2a
anusali04928 <alianus74@gmail.com>
2020-06-20 18:48:57 +0500

    inti fixed

--- rope.py
+++ rope.py
@@ -1,27 +1,30 @@
 class Rope(object):
-    def __init__(self, data=''):
+    def __init__(self, data='', parent=None):
         # checks if input is a string
         if isinstance(data, list):
             if len(data) == 0:
                 self.__init__()
             elif len(data) == 1:
-                self.__init__(data[0])
+                self.__init__(data[0],parent=parent)
             else:
+                self.current = self
                 # Round-up division (to match rope arithmetic associativity)
                 idiv = len(data) // 2 + (len(data) % 2 > 0)
-                self.left = Rope(data[:idiv])
-                self.right = Rope(data[idiv:])
+                self.left = Rope(data[:idiv],parent=self.current)
+                self.right = Rope(data[idiv:],parent=self.current)
+                self.parent=parent 
                 self.data = ''
                 self.weight = len(self.data.join(data[:idiv]))
         elif isinstance(data, str):
             self.left = None
             self.right = None
-            self.data = data
+            self.data = data+' '
             self.weight = len(data)
+            self.parent=parent 
         else:
             raise TypeError('Only strings are currently supported')
         # Word iteration
-        self.current = self
+
 
     # checks if tree is balanced
     # def __eq__(self, other):
@@ -46,6 +49,15 @@ class Rope(object):
             return self.search(node.left, i)
         return node.data[i]
 
+    def searchnode(self, node, i):
+        if node.weight==i:
+            return (node)
+        else:
+            if node.weight < i and node.right != None:
+                return self.searchnode(node.right, i-node.weight)
+            elif node.left != None:
+                return self.searchnode(node.left, i)
+
     def concatenation(self, newrope, length, node1, node2):
 
         self.left = node1
@@ -60,20 +72,21 @@ class Rope(object):
     #     newnode2 = Rope(datar[splitindex:])
     #     print(newnode1.data)
 
-    def searchnode(self, node, i):
-        if node.weight < i and node.right != None:
-            return self.searchnode(node.right, i-node.weight)
-        elif node.left != None:
-            return self.searchnode(node.left, i)
-        return(node)
 
-    # def split(self, node, index):
-    #     if node.left == None:
-    #         newrope = Rope()
-    #         newrope.left = None
-    #         datarr = node.data.split()
-    #         newrope.data = datarr[:index]
-    #         newrope.weight = node.weight
+
+    def split(self,rootnode, index):
+         target=searchnode(rootnode,index)
+         ##case01
+         if target.p==None:
+             return target.left,target.right
+         if target.p!=None:
+
+             right_tree=Rope()
+
+
+
+         
+         
 
 
 phrase = "This code is by Aaron"
@@ -86,5 +99,5 @@ rope2 = Rope(array_phrase2)
 # length_left = len(phrase)-len(array_phrase)+1
 # r = newrope.concatenation(newrope.current, length_left, rope, rope2)
 # print(r.search(r.current, 32)
-somenode = Rope()
 somenode = rope2.searchnode(rope2.current, 6)
+print(somenode.data)
---
+++ tasks.md
@@ -0,0 +1,8 @@
+### Data Structures & Algorithms 
+
+
+
+
+- Search the kth element node.
+- Set its parent G's right child to none and go to the parent of G and subtract the weight of k from it.
+- Go for successive nodes an
